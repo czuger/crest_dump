05c5edf412e8785937bde5974039a92adc34e426
Zuger Cédric <zuger.cedric@gmail.com>
2016-01-04 10:56:14 +0100

    Added symlinks for www exposition + removing old dumps.

--- dumper.rb
+++ dumper.rb
@@ -14,31 +14,52 @@ f = File.open( config_file, 'r' )
 db_config = YAML::load( f )
 pl = ProcessedLines.new
 
+# Loading rails database config
 database = db_config[ ARGV[1] ][ 'database' ]
 pg_config = {}
 pg_config[ :user ] = db_config[ ARGV[1] ][ 'username' ] if db_config[ ARGV[1] ][ 'username' ]
 pg_config[ :password ] = db_config[ ARGV[1] ][ 'password' ] if db_config[ ARGV[1] ][ 'password' ]
 pg_config[ :search_path ] = db_config[ ARGV[1] ][ 'schema_search_path' ] if db_config[ ARGV[1] ][ 'schema_search_path' ]
 
-pp pg_config
-
 DB = Sequel.postgres( database, pg_config )
 
 fields = [ :cpp_region_id, :cpp_eve_item_id, :history_date, :order_count, :volume, :low_price, :avg_price, :high_price ]
 
-File.open( 'crest_dump.csv', 'w' ) do |file|
+# Dumping crest table
+dump_time = Time.now
+dump_path = "dumps/crest_dump_#{dump_time.to_i}.csv"
+File.open( dump_path, 'w' ) do |file|
   DB[ :regions ].join( :crest_price_histories, region_id: :id ).join( :eve_items, id: :eve_item_id ).
     use_cursor( hold: false ).select( *fields ).each do |row|
     row[ :history_date ] = row[ :history_date ].utc.to_i
     array = fields.map{ |e| row[ e ] }
     file.puts( array.join( ';' ) )
     pl.p( nb_lines_trigger: 100000 )
+    break
   end
 end
 
-`gzip crest_dump.csv`
+# Zipping file
+`gzip #{dump_path}`
+`echo \`md5sum #{dump_path}.gz | awk '{ print $1 }'\` > #{dump_path}.md5`
 
-`echo \`md5sum dump.csv.gz\` > dump.md5`
+# Changing symlinks
+File.unlink( 'www/crest_dump.csv.gz' ) if File.exist?( 'www/crest_dump.csv.gz' )
+File.unlink( 'www/crest_dump.csv.gz.md5' ) if File.exist?( 'www/crest_dump.csv.gz.md5' )
+
+File.symlink( "../#{dump_path}.gz", 'www/crest_dump.csv.gz' )
+File.symlink( "../#{dump_path}.md5", 'www/crest_dump.csv.gz.md5' )
+
+# Removing old files
+Dir.chdir( 'dumps' )
+Dir.entries( '.' ).each do |entry|
+  next if entry == 'keep_dir'
+  if File.file?( entry )
+    if File.ctime( entry ) < dump_time
+      File.unlink( entry )
+    end
+  end
+end
 
 
 
